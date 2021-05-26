829b1ef581627b10314a243a163752a5af1b33f2
LaurenWash <>
2021-05-26 19:34:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let pluralNoun = "Your Momma";
+let name = "Karen";
+let verb = "be funky";
+let adjective = "pompous";
+let color = "lavender";
+
+console.log(`Javascript provides a ${color} collection of tools - including ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} with strings.`)
+//console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.");
